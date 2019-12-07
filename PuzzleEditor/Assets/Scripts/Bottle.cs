e26bf0710ed5889ef2ceada31b626d916de18832
weiqiuxiang <weiqiuxiang2010@gmail.com>
2019-12-07 15:53:46 +0900

    DoTween导入 部分代码上传

---
+++ PuzzleEditor/Assets/Scripts/Bottle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Bottle : MonoBehaviour
+    {
+        public int maxAmount { get; private set; }
+        public int currentAmount { get; private set; }
+
+        /// <summary>
+        /// 加水/倒水
+        /// </summary>
+        /// <param name="amount">加水(正数)/倒水(负数)的量</param>
+        /// <returns>true : 加水/倒水成功</returns>
+        public bool AddWater(int amount)
+        {
+            int restAmount = this.currentAmount + amount;
+            if ( (restAmount < 0) || (restAmount > maxAmount) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- PuzzleEditor/Assets/Scripts/PuzzleManager.cs
+++ PuzzleEditor/Assets/Scripts/PuzzleManager.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class PuzzleManager : MonoBehaviour
+namespace Game
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum PuzzleManagerState
     {
-        
+        Init = 1,               //初期化
+        SelectBottle = 2,       //选择杯子
+        PourWaterAnimation = 3, //倒水动画
+        VictoryCheck = 4,       //胜利判定
     }
 
-    // Update is called once per frame
-    void Update()
+    public class PuzzleManager : MonoBehaviour
     {
-        
+        public UnityEvent OnVictory = null;         //谜题解开时的callback
+        private int bottleAmount = 0;               //杯子数量
+        private List<Bottle> bottleList = null;     //杯子列表
+        public List<Bottle> BottleList => (bottleList ?? (bottleList = new List<Bottle>()));
+        private int victoryBottleId = 0;            //胜利条件中的杯子id
+        private int victoryAmount = 0;              //胜利条件中的杯子容量
+
+        void OnEnable()
+        {
+
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //GameLoop
+
+        }
     }
 }
